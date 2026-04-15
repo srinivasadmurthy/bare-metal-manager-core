9593a24d288a4c87c5d05e011457955cc8b9d958
Srinivasa Murthy <srmurthy@nvidia.com>
2026-04-15 19:36:52 +0000

    WIP
    Signed-off-by: Srinivasa Murthy <srmurthy@nvidia.com>

---
+++ crates/api-db/migrations/20260413193743_spx_partitions.sql
@@ -0,0 +1,14 @@
+-- Add migration script here
+CREATE TABLE IF NOT EXISTS spx_partitions
+(
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+
+    name VARCHAR(256) NOT NULL DEFAULT (''),
+    description VARCHAR(1024) NOT NULL DEFAULT (''),
+    tenant_organization_id VARCHAR(64) NOT NULL,
+    config_version VARCHAR(64) NOT NULL,
+
+    created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    deleted TIMESTAMPTZ
+);
